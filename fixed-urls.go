e3babee83808648e12fff6b0eb5c6bc8fd541367
Mac Taylor <cslag@hotmail.com.tw>
2022-07-01 23:16:20 +0800

    fix auth url

--- fixed-urls.go
+++ fixed-urls.go
@@ -1,6 +1,6 @@
 package shopeego
 
 func init() {
-	availablePaths["GetAccessToken"] = "auth/token/get"
-	availablePaths["RefreshAccessToken"] = "auth/access_token/get"
+	availablePaths["GetAccessToken"] = "/auth/token/get"
+	availablePaths["RefreshAccessToken"] = "/auth/access_token/get"
 }
